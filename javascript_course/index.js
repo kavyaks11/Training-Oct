828bc8a1fe2e65e18eb2b429229b7c21919fb8ea
kavyaks <kavya.ks@vonnue.com>
2022-12-13 09:51:57 +0530

    feat: section 2 completed

--- javascript_course/index.js
+++ javascript_course/index.js
@@ -273,7 +273,7 @@ console.log(lastName);   */
 
 // coding challenge 3
 
-let avgScoreDolphins, avgScoreKoalas;
+// let avgScoreDolphins, avgScoreKoalas;
 // avgScoreDolphins = (96 + 108 + 89) / 3;
 // avgScoreKoalas = (88 + 91 + 110) / 3;
 
@@ -318,42 +318,66 @@ let avgScoreDolphins, avgScoreKoalas;
 // }
 
 
-const day = 'sunday';
+// const day = 'sunday';
 
-switch (day) {
-    case 'monday ':
-        console.log('Plan course structure');
-        console.log('Go to coding meetup');
-        break;
-    case 'tuesday':
-        console.log('Prepare theory videos');
-        break;
-    case 'wednesday':
-    case 'thursday':
-        console.log('Write code examples');
-        break;
-    case 'friday':
-        console.log('Record videos');
-        break;
-    case 'saturday':
-    case 'sunday':
-        console.log('Enjoy the weekend');
-        break;
-    dafault:
-        console.log('not a valid day!');    
-}
+// switch (day) {
+//     case 'monday ':
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples');
+//         break;
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend');
+//         break;
+//     dafault:
+//         console.log('not a valid day!');    
+// }
 
-if(day === 'monday'){
-    console.log('plan course structure');
-    console.log('Go to coding meetup');
-} else if(day === 'tuesday') {
-    console.log('Prepare theory videos');
-} else if(day === 'wednesday' || day === 'thursday'){
-    console.log('write coding examples');
-} else if(day === 'friday'){
-    console.log('Record Videos');
-} else if(day === 'saturday' || day === 'sunday'){
-    console.log('Enjoy the weekend');
-} else {
-    console.log('Not a valid day');
-}
+// if(day === 'monday'){
+//     console.log('plan course structure');
+//     console.log('Go to coding meetup');
+// } else if(day === 'tuesday') {
+//     console.log('Prepare theory videos');
+// } else if(day === 'wednesday' || day === 'thursday'){
+//     console.log('write coding examples');
+// } else if(day === 'friday'){
+//     console.log('Record Videos');
+// } else if(day === 'saturday' || day === 'sunday'){
+//     console.log('Enjoy the weekend');
+// } else {
+//     console.log('Not a valid day');
+// }
+
+// const age = 14;
+// age >= 18 ? console.log('I like to drink wine'):
+// console.log('I like to drink water');
+
+// const drink = age >= 18 ? 'wine' : 'water';
+// console.log(drink);
+
+// let drink2;
+// if (age >= 18){
+//     drink2 = 'wine';
+// } else {
+//     drink2 = 'water';
+// }
+// console.log(drink2 );
+
+// coding challen ge 4
+let tip, bill;
+bill = 275;
+bill >= 50 && bill <= 300 ? tip = bill * (15 / 100):
+tip = bill * (20 / 100) ;
+console.log(tip);
+
+console.log(`bill value is ${bill}, the tip is ${tip}, and the total value is ${bill + tip}`);
