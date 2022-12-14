f4e4dd6fbc5add9f2fa87715807b5619a6151ff9
kavyaks <kavya.ks@vonnue.com>
2022-12-14 20:00:26 +0530

    feat:section7 in progress

---
+++ javascript_course/section7/guess_my_number/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
