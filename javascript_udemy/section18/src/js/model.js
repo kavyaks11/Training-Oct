1a6a2fbe493bf1894e29453cf0397cc5067809d3
kavyaks <kavya.ks@vonnue.com>
2022-12-31 14:25:33 +0530

    testing

---
+++ javascript_udemy/section18/src/js/model.js
@@ -0,0 +1,35 @@
+import { async } from "regenerator-runtime";
+
+export const state = {
+  recipe: {},
+};
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/get?rId=${id}`
+    //   `https://forkify-api.herokuapp.com/api/get?rId=47161`
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    console.log(res, data);
+
+    const { recipe } = data; //since data contains underscore , destructuring the data,
+
+    state.recipe = {
+      id: recipe.recipe_id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
