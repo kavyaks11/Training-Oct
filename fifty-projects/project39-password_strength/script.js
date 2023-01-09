5af14607f8bec8b5a1b35aed4cfe1f7bd0d79a85
kavyaks <kavya.ks@vonnue.com>
2023-01-09 18:48:39 +0530

    feat:completed

---
+++ fifty-projects/project39-password_strength/script.js
@@ -0,0 +1,11 @@
+const password = document.getElementById("password");
+const background = document.querySelector(".background");
+
+password.addEventListener("input", (e) => {
+  const input = e.target.value;
+  //   console.log(input);
+  const length = input.length;
+  //   console.log(20 - length * 2);
+  const blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`;
+});
